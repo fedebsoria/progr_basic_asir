c67fdded10680cb12896ac47cdc834a118595615
Fede Soria <fsoria7@outlook.com>
2025-05-13 14:51:00 +0200

    create class Persona and class  Estudiante

--- tarea_31/Estudiante.java
+++ tarea_31/Estudiante.java
@@ -1,3 +1,28 @@
-public class Estudiante {
-    
+public class Estudiante extends Persona{
+    private String carrera;
+	private  long legajo;
+
+	public Estudiante(String name, int age, String carrera, long legajo) {
+		super(name, age);
+        this.carrera = carrera;
+        this.legajo = legajo;
+	}
+
+    //metodos carrera
+    public String getCarrera(){
+        return this.carrera;
+    }
+
+    public void setCarrera(String val){
+        this.carrera = val;
+    }
+
+    //metodos legajo
+    public long getLegajo(){
+        return this.legajo;
+    }
+
+    public void setLegajo(long val){
+        this.legajo = val;
+    }
 }
---
+++ tarea_31/Persona.java
@@ -0,0 +1,35 @@
+/*
+ * Práctica Clases Extendidas
+ * 
+ * Alumno: Federico Soria
+ * 
+ */
+
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    //Constructor: inicializa Persona
+    public Persona(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    //metodos nombre
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setNombre(String val){
+        this.nombre = val;
+    }
+
+    //metodos edad
+    public int getEdad(){
+        return this.edad;
+    }
+
+    public void setEdad(int val){
+        this.edad = val;
+    }
+}
